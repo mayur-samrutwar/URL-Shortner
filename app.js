6dfb4f3b6dfbfea94ad0e517b0da3ed42a11a7a6
mayur-samrutwar <samrutwarmayur1@gmail.com>
2021-04-11 13:57:59 +0530

    added Procfile for Heroku

--- app.js
+++ app.js
@@ -134,6 +134,6 @@ app.post("/", function (req, res){
 // PORT Listen
 //
 //
-app.listen(3000, function (){
+app.listen(process.env.PORT || 3000, function (){
     console.log("Server running on port 3000");
 });
